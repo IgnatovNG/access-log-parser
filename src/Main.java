519cd16a064fcff5682aa6a7c50f18cf202ee564
Ignatov-NG <127387123+Ignatov-NG@users.noreply.github.com>
2024-02-24 16:01:32 +0300

    Добавлена логика операций с числами

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите значение 1 и нажмите <Enter> :");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите значение 2 и нажмите <Enter> :");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма : " + (number + number2));
+        System.out.println("Разность : " + (number - number2));
+        System.out.println("Произведение : " + (number * number2));
+        System.out.println("Частное: " + ((double) number / number2));
     }
 }
